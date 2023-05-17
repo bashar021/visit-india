a0320aa77b6b5e90c966d7f8577b2defe47611bc
Bashar Alam <111270375+bashar021@users.noreply.github.com>
2023-05-17 11:50:17 +0530

    Update post.js

--- src/controlers/post.js
+++ src/controlers/post.js
@@ -14,7 +14,7 @@ async function Postdata(url, postdata) {
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'Access-Control-Allow-Origin':'http://localhost:3000/',
+//             'Access-Control-Allow-Origin':'http://localhost:3000/',
             // jwt token is used if we are using a token 
             // jwt:token
         },
